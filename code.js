daf2d6a99a72c8ad2e8c978a6f7fd605429f4050
lightrav3n <135107438+lightrav3n@users.noreply.github.com>
2024-01-27 12:04:33 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,56 @@
 window.onload = function () {
-  // Your script code here
+  // swipe
+  const bigCard = document.getElementById("bigCard");
+  let touchStartX = 0;
+  let touchEndX = 0;
 
+  bigCard.addEventListener("touchstart", handleTouchStart, false);
+  bigCard.addEventListener("touchmove", handleTouchMove, false);
+  bigCard.addEventListener("mousedown", handleMouseDown, false);
+
+  function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+  }
+
+  function handleTouchMove(event) {
+    touchEndX = event.touches[0].clientX;
+    handleSwipe();
+  }
+
+  function handleMouseDown(event) {
+    touchStartX = event.clientX;
+    document.addEventListener("mousemove", handleMouseMove, false);
+    document.addEventListener("mouseup", handleMouseUp, false);
+  }
+
+  function handleMouseMove(event) {
+    touchEndX = event.clientX;
+    handleSwipe();
+  }
+
+  function handleMouseUp() {
+    document.removeEventListener("mousemove", handleMouseMove, false);
+    document.removeEventListener("mouseup", handleMouseUp, false);
+  }
+
+  function handleSwipe() {
+    const swipeThreshold = 150;
+
+    if (touchStartX - touchEndX > swipeThreshold) {
+      setIndex((currentIndex % 3) + 1);
+    } else if (touchEndX - touchStartX > swipeThreshold) {
+      setIndex((currentIndex + 1) % 3 || 3);
+    }
+  }
+
+  // rest
   const radio1 = document.getElementById("radio1");
   const radio2 = document.getElementById("radio2");
   const radio3 = document.getElementById("radio3");
-  const bigCard = document.getElementById("bigCard");
+  
   const cardText = document.getElementById("cardText");
   const cardTitle = document.getElementById("cardTitle");
+
   let currentIndex = 1;
   let intervalId = setInterval(changeImage, 8000);
 
